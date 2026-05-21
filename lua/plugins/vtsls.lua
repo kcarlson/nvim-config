21f584acae8b4804db7885da1722d75c1b6533ef
Kristoffer Carlson <kristoffer.carlson@bambuser.com>
2026-05-21 14:06:50 +0200

    plugins: Disable vtsls inlay hints to work around TypeScript 5.8.3 bug

---
+++ lua/plugins/vtsls.lua
@@ -0,0 +1,35 @@
+-- Workaround for TypeScript 5.8.3 bug: inlay hints crash on ComputedPropertyName nodes.
+-- Remove once the project upgrades to a TypeScript version that fixes this assertion failure.
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                parameterNames = { enabled = "none" },
+                parameterTypes = { enabled = false },
+                variableTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+                enumMemberValues = { enabled = false },
+              },
+            },
+            javascript = {
+              inlayHints = {
+                parameterNames = { enabled = "none" },
+                parameterTypes = { enabled = false },
+                variableTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+                enumMemberValues = { enabled = false },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
